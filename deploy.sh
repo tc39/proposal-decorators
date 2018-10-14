9e1056090a2ec42d1c252398886901f6e1d17ec6
Daniel Ehrenberg <littledan@chromium.org>
2018-10-14 20:16:14 +0200

    More fault-tolerant deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,24 @@
 
 set -ev
 
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [[ "$TRAVIS_BRANCH" != master ]]; then
+  echo "Skipping deploy; just doing a build."
+  npm run build
+  exit 0
+fi
+
 # Enable SSH authentication
 
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+if [[ $ENCRYPTED_KEY == "" ]]; then
+    echo "Auto-deploy GitHub key missing; exiting without a build"
+    exit 0
+fi
 
 $(npm bin)/set-up-ssh --key "$ENCRYPTED_KEY" \
                       --iv "$ENCRYPTED_IV" \
